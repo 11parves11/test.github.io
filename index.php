02a900cafa08d4738a5bc6ba6771abb64fea8e83
11parves11 <56088126+11parves11@users.noreply.github.com>
2020-06-30 11:57:01 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project 1v2</title>
+    <link rel="shortcut icon" type="image/png" href="img/fav.png">  
+    
+ 
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;800&display=swap" rel="stylesheet">
+ 
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+ 
+  <link rel="stylesheet" href="css/custom.css"> 
+</head>
+<body>
+  <!--header-->
+  <header>
+
+    <div class="head-cover">
+        <img src="img/hc.png" alt="" class="hc">
+    </div>
+    <div class="logo">
+
+        <img src="img/logo.png" alt="" class="logo">
+    </div>
+
+    <div class="menu">
+        <ul>
+           <a href="#"><li>Home</li></a>
+           <a href="#"><li>Our Menu</li></a>
+           <a href="#"><li>Help</li></a>
+           <a href="#"><li>Contact Us</li></a>
+
+
+
+
+        </ul>
+    </div>
+
+    <div class="menu-img">
+      <img src="img/51.png" alt="">
+    </div>
+     <div class="header-text">
+       <h2>Healthy Food </h2>
+     
+       <h2>Resturent </h2>
+       <h4>You can get your favourite </h4>
+     
+       <h4>set menu.So hurry Up!</h4>
+     </div>
+  
+
+
+  </header>
+  <!--header END-->
+
+  <!--section 1-->
+  <section class="sec1">
+    <div class="sec-title"><h1>POPULAR MENU</h1></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="menu-1">
+            <img src="img/1.png" alt="" class="one">
+            <h2 style="padding-top: 10px;">NOODLES</h2>
+            <p>We provide high Quality noodles at resonable price.</p>
+            <h2>Price:5$</h2>
+          
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="menu-2">
+
+            <img src="img/2.png" alt="" class="two">
+            <h2 style="padding-top: 10px;">NOODLES</h2>
+            <p>We provide high Quality noodles at resonable price.</p>
+            <h2>Price:5$</h2>
+            
+          </div>
+        </div>
+        <div class="col-md-4">    
+           <div class="menu-3">
+           
+            <img src="img/3.png" alt="" class="three">
+            <h2 style="padding-top: 10px;">NOODLES</h2>
+            <p>We provide high Quality noodles at resonable price.</p>
+            <h2>Price:5$</h2>
+           </div>
+          </div>
+
+      </div>
+    </div>
+    
+    
+
+
+  </section>
+
+
+  <!--section 1 end-->
+
+
+  <script src="js/jquery3.4.1.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
