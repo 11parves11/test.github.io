3fa03d3cefafab233ff304816c8f266e93592bba
11parves11 <56088126+11parves11@users.noreply.github.com>
2020-06-30 11:54:47 +0600

    new test

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$(".menu-img").click(function(){
+
+  $(".menu").toggleClass("show-menu")
+  $(".menu-img").toggleClass("butt-rotate")
+
+
+
+
+
+})
+
+
+
+
+
+
+
+});
